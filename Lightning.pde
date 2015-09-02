68d49c294be9d2c72425200d1ec419cfa15751c4
unknown <thwong2@697-334-Pippin.sf.k12.internal>
2015-09-02 10:45:19 -0700

    Updated some things, maybe done

--- Lightning.pde
+++ Lightning.pde
@@ -1,37 +1,51 @@
-int startX = 0;
-int startY=150;
+int startX = mouseX;
+int startY=mouseY;
 int endX = 0;
 int endY = 150;
 int lightningDrawn;
 void setup()
 {
-  background(0);
+  background(255);
   strokeWeight(2);
-    size(300,300);
+  size(300,300);
 
 }
 void draw()
 {
-  stroke(randomColor());
-  while( endX < 300){
-    endX = startX + (int)(Math.random()*10);
-    endY= startY+(int)(Math.random()*20)-10;
-    line(startX,startY,endX,endY);
-    startX=endX;
-    startY =endY;
-  }
+
 }
 void mousePressed()
 {
-  startX=0;
-  startY=150;
-  endX=0;
-  endY=150;
+	drawLightning();
 }
 color randomColor(){
-  return color((int)(Math.random()*255),(int)(Math.random()*255),
+  	return color((int)(Math.random()*255),(int)(Math.random()*255),
     (int)(Math.random()*255));
 }
-void numberDrawn(){
-
+void clear(){
+	background(255);
+}
+void keyPressed(){
+	if(	key == 'a'){
+		clear();
+	}
+}
+void drawLightning(){
+	startX=mouseX;
+  	startY=mouseY;
+  	endX=0;
+	for(int x = 0;x < (int)(Math.random()*10);x++){
+	stroke(randomColor());
+  	while( endX < 300)
+  	{
+   		endX = startX + (int)(Math.random()*10);
+    	endY= startY+(int)(Math.random()*20)-10;
+    	line(startX,startY,endX,endY);
+    	startX=endX;
+    	startY =endY;
+  	}
+  	startX=mouseX;
+  	startY=mouseY;
+  	endX=0;
+  }	
 }
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,37 +14,54 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int startX = 0;
-int startY=150;
+int startX = mouseX;
+int startY=mouseY;
 int endX = 0;
 int endY = 150;
-int r = (int)(Math.random()*256);
-int g = (int)(Math.random()*256);
-int b = (int)(Math.random()*256);
+int lightningDrawn;
 public void setup()
 {
-	background(0);
-	strokeWeight(2);
-  	size(300,300);
+  background(255);
+  strokeWeight(2);
+    size(300,300);
 
 }
 public void draw()
 {
-	stroke(255,255,0);
-	while( endX < 300){
-		endX = startX + (int)(Math.random()*10);
-		endY= startY+(int)(Math.random()*20)-10;
-		line(startX,startY,endX,endY);
-		startX=endX;
-		startY =endY;
-	}
+
 }
 public void mousePressed()
 {
-	startX=0;
-	startY=150;
-	endX=0;
-	endY=150;
+	drawLightning();
+}
+public int randomColor(){
+  return color((int)(Math.random()*255),(int)(Math.random()*255),
+    (int)(Math.random()*255));
+}
+public void clear(){
+	background(255);
+}
+public void keyPressed(){
+	if(	key == 'a'){
+		clear();
+	}
+}
+public void drawLightning(){
+	startX=mouseX;
+  	startY=mouseY;
+  	endX=0;
+	for(int x = 0;x < (int)(Math.random()*10);x++){
+	stroke(randomColor());
+  	while( endX < 300)
+  	{
+   		endX = startX + (int)(Math.random()*10);
+    	endY= startY+(int)(Math.random()*20)-10;
+    	line(startX,startY,endX,endY);
+    	startX=endX;
+    	startY =endY;
+  	}
+  	endX=0;
+  }	
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
